67b2b1e26fcc9fa376c9b7207a3397e859230f93
Pablo Degryse <pablo.degryse@student.howest.be>
2016-12-09 09:59:03 +0100

    ded
    ezdzd

--- serverModules/StaticServer.js
+++ serverModules/StaticServer.js
@@ -9,9 +9,9 @@ let StaticServer=(function () {
         port=myPort;
         app=express();
 
-        mongoose.connect('localhost:27017/pictionar-e');
+        //mongoose.connect('localhost:27017/pictionar-e');
 
-        //mongoose.connect('mongodb://testUser:testuser@ds159387.mlab.com:59387/pictionar-e');
+        mongoose.connect('mongodb://testUser:testuser@ds159387.mlab.com:59387/pictionar-e');
         setupHttpServer();
         setupExpress();
         SocketHandler.init(server);
